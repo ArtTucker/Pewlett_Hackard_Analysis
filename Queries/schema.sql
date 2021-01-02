dbabfba2ef3eca933209020eea88ecf6f7565a0f
Art Tucker <art.w.tucker@gmail.com>
2021-01-02 15:01:14 -0800

    Update schema.sql

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -58,7 +58,9 @@ CREATE TABLE titles (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, title, from_date)
 );
 
 SELECT * FROM departments;
+
+SELECT * FROM titles;
