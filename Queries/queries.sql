2d17b44761829d4831f9d90edeb747114b739836
Art Tucker <art.w.tucker@gmail.com>
2021-01-04 17:36:06 -0800

    Update queries.sql

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -75,3 +75,28 @@ FROM retirement_info AS ri
 LEFT JOIN dept_emp AS de
 ON ri.emp_no = de.emp_no;
 
+SELECT ri.emp_no,
+	ri.first_name,
+    ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info AS ri
+LEFT JOIN dept_emp AS de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+COPY (SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no)
+TO '';
