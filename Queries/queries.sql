fcb0740f40f70f1bc3b2d99bc066906385c0020c
Art Tucker <art.w.tucker@gmail.com>
2021-01-04 18:46:18 -0800

    Update queries.sql

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -93,10 +93,10 @@ ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no
 ORDER BY de.dept_no;
 
-COPY (SELECT COUNT(ce.emp_no), de.dept_no
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO curr_emp_by_dept_no
 FROM current_emp as ce
 LEFT JOIN dept_emp as de
 ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no
-ORDER BY de.dept_no)
-TO '';
+ORDER BY de.dept_no;
