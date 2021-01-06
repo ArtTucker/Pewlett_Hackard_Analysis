161ef5cf9c0d1426b00b59e147bd0f0dd6ea6f52
Art Tucker <art.w.tucker@gmail.com>
2021-01-05 23:53:39 -0800

    Update Employee_Database_challenge.sql

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,1 +1,13 @@
 -- Employee Database Challenge
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	ti.title,
+	ti.from_date,
+	ti.to_date
+INTO retirement_titles
+FROM employees AS e
+	INNER JOIN titles AS ti
+		ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
